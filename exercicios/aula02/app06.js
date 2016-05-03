154b4e1bff2279b21702cb7d18be9290233411fd
Jackson F. de A. Mafra <jacksonfdam@gmail.com>
2016-05-02 22:25:27 -0300

    Update app06.js

--- exercicios/aula02/app06.js
+++ exercicios/aula02/app06.js
@@ -4,14 +4,19 @@ var fs = require('fs');
 
 var server = http.createServer(function(request, response) {
     fs.readFile(__dirname + '/estados-cidades.json', function(err, arquivo) {
-        response.writeHeader(200, {
-            'Content-Type': 'application/json'
-        });
-        response.write(arquivo);
+        var headers = {};
+	    headers["Access-Control-Allow-Origin"] = "*";
+	    headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+	    headers["Access-Control-Allow-Credentials"] = true;
+	    headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+	    headers["Access-Control-Allow-Headers"] = "X-Requested-With, Access-Control-Allow-Origin, X-HTTP-Method-Override, Content-Type, Authorization, Accept";
+	    headers['Content-Type'] = 'application/json';
+	    response.writeHead(200, headers);    
+	    response.write(arquivo);
         response.end();
     });
 });
 
-server.listen(3000, function() {
+server.listen(8000, function() {
     console.log('Executando Servidor HTTP');
 });
