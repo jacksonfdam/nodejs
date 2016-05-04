0271771b88dcc36463c8bab30ccf73b655b154b1
Jackson F. de A. Mafra <jacksonfdam@gmail.com>
2016-05-03 21:30:43 -0300

    Create bandeiras.js

---
+++ exercicios/aula02/lab0205/bandeiras.js
@@ -0,0 +1,132 @@
+[
+  {
+    "codigo": 1,
+    "titulo": "Acre",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-acre-105x74.png"
+  },
+  {
+    "codigo": 2,
+    "titulo": "Alagoas",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-alagoas-105x70.png"
+  },
+  {
+    "codigo": 3,
+    "titulo": "Amapá",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-amapa-105x74.png"
+  },
+  {
+    "codigo": 4,
+    "titulo": "Amazonas",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-amazonas-105x75.png"
+  },
+  {
+    "codigo": 5,
+    "titulo": "Bahia",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-bahia-105x70.png"
+  },
+  {
+    "codigo": 6,
+    "titulo": "Ceará",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-ceara-105x74.png"
+  },
+  {
+    "codigo": 7,
+    "titulo": "Espírito Santo",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-espirito-santo-105x74.png"
+  },
+  {
+    "codigo": 8,
+    "titulo": "Goiás",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-goias-105x75.png"
+  },
+  {
+    "codigo": 9,
+    "titulo": "Maranhão",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-maranhao-105x70.png"
+  },
+  {
+    "codigo": 10,
+    "titulo": "Mato Grosso",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-mato-grosso-105x74.png"
+  },
+  {
+    "codigo": 11,
+    "titulo": "Mato Grosso do Sul",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-mato-grosso-do-sul-105x74.png"
+  },
+  {
+    "codigo": 12,
+    "titulo": "Minas Gerais",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-minas-gerais-105x74.png"
+  },
+  {
+    "codigo": 13,
+    "titulo": "Pará",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-para-105x70.png"
+  },
+  {
+    "codigo": 14,
+    "titulo": "Paraíba",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-paraiba-105x74.png"
+  },
+  {
+    "codigo": 15,
+    "titulo": "Paraná",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-parana-105x74.png"
+  },
+  {
+    "codigo": 16,
+    "titulo": "Pernambuco",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-pernambuco-105x74.png"
+  },
+  {
+    "codigo": 17,
+    "titulo": "Piauí",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-piaui-105x70.png"
+  },
+  {
+    "codigo": 18,
+    "titulo": "Rio de Janeiro",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-rio-de-janeiro1-105x74.png"
+  },
+  {
+    "codigo": 19,
+    "titulo": "Rio Grande do Norte",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-rio-grande-do-norte-105x70.png"
+  },
+  {
+    "codigo": 20,
+    "titulo": "Rio Grande do Sul",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-rio-grande-do-sul-105x74.png"
+  },
+  {
+    "codigo": 21,
+    "titulo": "Rondônia",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-rondonia-105x74.png"
+  },
+  {
+    "codigo": 22,
+    "titulo": "Roraima",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-roraima-105x74.png"
+  },
+  {
+    "codigo": 23,
+    "titulo": "Santa Catarina",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-santa-catarina-105x74.png"
+  },
+  {
+    "codigo": 24,
+    "titulo": "São Paulo",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-sao-paulo1-105x70.png"
+  },
+  {
+    "codigo": 25,
+    "titulo": "Sergipe",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-sergipe-105x74.png"
+  },
+  {
+    "codigo": 26,
+    "titulo": "Tocantins",
+    "imagem": "http://www.estadosecapitaisdobrasil.com/wp-content/uploads/2014/09/bandeira-tocantins-105x74.png"
+  }
+]
