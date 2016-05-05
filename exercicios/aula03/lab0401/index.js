8e2834ede83780b9ae8f2d02422e4c8e5e252927
Jackson F. de A. Mafra <jacksonfdam@gmail.com>
2016-05-06 00:51:49 +0200

    Chat With Socket

---
+++ exercicios/aula03/lab0401/index.js
@@ -0,0 +1,28 @@
+var express = require('express');  
+var app = express();  
+var server = require('http').createServer(app);  
+var io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/bower_components'));  
+app.get('/', function(req, res,next) {  
+		    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(client) {  
+    console.log('Client connected...');
+
+    client.on('join', function(data) {
+        console.log(data);
+        client.emit('messages', 'Hello from server');
+    });
+
+    client.on('messages', function(data) {
+           client.emit('broad', data);
+           client.broadcast.emit('broad',data);
+    });
+
+
+});
+
+
+server.listen(4000);  
